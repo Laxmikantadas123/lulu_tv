f659ad3d3d2bf0886439c8ee090103c0bfbb0820
Laxmikantadas123 <daslaxmikanta711@gmail.com>
2024-09-23 18:10:11 +0530

    commit

---
+++ server/utils/asyncHandler.js
@@ -0,0 +1,12 @@
+const asyncHandel=(fn)=>async (req,res,next)=>{
+try {
+    await fn(req,res,next)
+} catch (error) {
+    res.status(500).json({
+        success:false,
+        message:error.message
+    })
+}
+}
+
+export default asyncHandel
