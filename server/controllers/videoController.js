f162b92ee5b3fdced2c025361e3623ec47c6fcd3
Laxmikantadas123 <daslaxmikanta711@gmail.com>
2024-09-24 17:25:34 +0530

    commit

--- server/controllers/videoController.js
+++ server/controllers/videoController.js
@@ -16,6 +16,22 @@ export const addVideo = asyncHandel(async (req, res) => {
 
 export const updateVideo = asyncHandel(async (req, res) => {
   try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      throw ApiError(404, "Video not found");
+    }
+    if (req.user.id === video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedVideo);
+    } else {
+      throw ApiError(403, "You can update only your video!");
+    }
   } catch (error) {
     throw ApiError(error.code, error.message);
   }
@@ -23,12 +39,52 @@ export const updateVideo = asyncHandel(async (req, res) => {
 
 export const getVideo = asyncHandel(async (req, res) => {
   try {
+    const video = await Video.findById(req.params.id);
+    res.status(200).json(video);
+  } catch (err) {
+    throw ApiError(err.code, err.message);
+  }
+});
+
+export const deleteVideo = asyncHandel(async (req, res) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) {
+      throw ApiError(404, "Video not found");
+    }
+    if (req.user.id === video.userId) {
+      await Video.findByIdAndDelete(req.params.id);
+      res.status(200).json("The video has been deleted.");
+    } else {
+      throw ApiError(403, "You can update only your video!");
+    }
   } catch (error) {
     throw ApiError(error.code, error.message);
   }
 });
 
-export const deleteVideo = asyncHandel(async (req, res) => {
+export const addView = asyncHandel(async (req, res) => {
+  try {
+  } catch (error) {
+    throw ApiError(error.code, error.message);
+  }
+});
+
+export const trend = asyncHandel(async (req, res) => {
+  try {
+  } catch (error) {
+    throw ApiError(error.code, error.message);
+  }
+});
+
+export const random = asyncHandel(async (req, res) => {
+  try {
+  } catch (error) {
+    throw ApiError(error.code, error.message);
+  }
+});
+
+export const sub = asyncHandel(async (req, res) => {
   try {
   } catch (error) {
     throw ApiError(error.code, error.message);
--- server/routers/videoRoute.js
+++ server/routers/videoRoute.js
@@ -6,11 +6,18 @@ import {
   updateVideo,
   getVideo,
   deleteVideo,
+  addView,
+  trend,
+  random,
+  sub
 } from "../controllers/videoController.js";
 
 router.post("/", authontication, addVideo);
 router.put("/:id", authontication, updateVideo);
 router.delete("/:id", authontication, deleteVideo);
 router.get("/search/:id", authontication, getVideo);
-
+router.put("/view/:id",addView)
+router.get("/trend",trend)
+router.get("/random",random)
+router.get("/sub",sub)
 export default router;
